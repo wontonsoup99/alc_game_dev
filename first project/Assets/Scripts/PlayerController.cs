ad099c4f06579c4f88032254c70f84fee1a9f103
Landon Powell <chase.powell456@gmail.com>
2020-09-24 08:23:32 -0600

    Changed git ignore added unity prototype 1 project

---
+++ first project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float turnSpeed = 60;
+    private float speed = 20;
+    private float horizontalInput;
+    private float forwardInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        // Moves the vehicle forward and back based on VerticalInput
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
+        // Moves the vehicle left and right based on horizontalInput
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime );
+        
+    }
+}
