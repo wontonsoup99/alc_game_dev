3eceac48398933178eb2875513be13b5a88a77f4
Landon Powell <chase.powell456@gmail.com>
2020-09-30 08:34:56 -0600

    finished challenge 1 and started on prototype 2

---
+++ challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    //sets the offset for the camera based off the plane
+    private Vector3 offset = new Vector3(30,0,10);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sets the camera to follow the planes position and adds an offset
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ challenge 1/Assets/Challenge 1/Scripts/RotatePropellor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePropellor : MonoBehaviour
+{
+    // sets the speed at which the propellor rotates
+    public float propellorSpeed = 1000;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Makes propellor spin constantly when the game is started
+        transform.Rotate(Vector3.forward, propellorSpeed * Time.deltaTime);
+    }
+}
