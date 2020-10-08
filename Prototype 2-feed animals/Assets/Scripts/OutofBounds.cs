36b382747ed7d978a6a26bdb180e697e06e113ee
Landon Powell <chase.powell456@gmail.com>
2020-10-08 09:07:59 -0600

    added move forward and limits to animals and food

---
+++ Prototype 2-feed animals/Assets/Scripts/OutofBounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutofBounds : MonoBehaviour
+{
+    public float topBounds = 30.0f;
+    public float lowerBounds = -15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+        }
+
+        if(transform.position.z < lowerBounds)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
