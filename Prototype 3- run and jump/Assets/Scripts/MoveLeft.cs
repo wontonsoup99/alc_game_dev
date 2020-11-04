ca9cac3c9ac66fd9c3aeb1333b61934686602776
Landon Powell <chase.powell456@gmail.com>
2020-11-04 09:05:37 -0700

    added prefabs and spawn manager to prototype 3

---
+++ Prototype 3- run and jump/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 30;
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves the GameObject left at a set speed
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
---
+++ Prototype 3- run and jump/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    private float startDelay = 2;
+    private float repeatDelay = 2;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
