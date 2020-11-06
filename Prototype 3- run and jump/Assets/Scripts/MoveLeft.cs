a0c449dafeeb933387f6d7cda0c31ba9f4ce9009
Landon Powell <chase.powell456@gmail.com>
2020-11-06 09:09:35 -0700

    added repeat background and more

--- Prototype 3- run and jump/Assets/Scripts/MoveLeft.cs
+++ Prototype 3- run and jump/Assets/Scripts/MoveLeft.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     public float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
+
+    private void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        //Moves the GameObject left at a set speed
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+         {
+            Destroy(gameObject);
+        }
+         if (playerControllerScript.gameOver == false)
+        {
+            //Moves the GameObject left at a set speed
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
     }
 }
