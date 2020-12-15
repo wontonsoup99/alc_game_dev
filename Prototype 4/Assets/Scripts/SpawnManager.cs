924173e80dd8291a823bb310303306282373a383
Landon Powell <chase.powell456@gmail.com>
2020-12-15 09:06:18 -0700

    added spawn manager to prototype 4

---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randonPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
